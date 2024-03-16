6584248b4f94b291c32aecd140b12addd0cb79f4
Leo Gott <lgott229@gmail.com>
2024-03-16 13:34:51 +0000

    basic setup done

--- cwk/client/Client.java
+++ cwk/client/Client.java
@@ -1,6 +1,55 @@
+import java.io.*;
+import java.net.*;
+
 public class Client 
-{
+{	
+	String command = null;
+	String fName = null;
+
+	public void Client(){
+
+		try{
+			int localhost_port = 9111;
+			Socket socket = new Socket( "localhost", localhost_port );
+
+			//create output
+			socketOutput = new PrintWriter(socket.getOutputStream(), true);
+			//create input
+			socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+		}
+		catch(IOExeption IOE){
+			System.out.println("IOExpection: %s", IOE);
+		}
+		catch(UnknownHostException UHE){
+			System.out.println("UnknownHostException: %s", UHE);
+		}
+
+		//creates request
+		String request = String.format("%s", command);
+		if(command == "put"){
+			request += String.format(" %s", fName);
+		}
+		
+		//send request to server
+		socketOutput.println(request);
+
+	}
+
 	public static void main( String[] args )
 	{
+		if(args[0] == "list"){
+			command = args[0]
+		}
+		else if(args[0] == "put"){
+			command = args[0]
+			fName = args[1];
+		}
+		else{
+			System.out.println("Invalid arguments.");
+		}
+
+		Client client = new Client();
+		client.Client();
 	}
 }
--- cwk/server/Server.java
+++ cwk/server/Server.java
@@ -1,6 +1,120 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+import java.util.*;
+
 public class Server 
-{
+{	
+	public void createNewFile(String filename){
+		try {
+			File file = new File(filename);
+			if (file.createNewFile()) {
+				System.out.println("File created: " + file.getName());
+			} 
+			else {
+				System.out.println("File already exists.");
+			}
+
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+	}
+
+	private static List<String> listFilesInDirectory(String directoryPath) {
+        List<String> fileList = new ArrayList<>();
+        File directory = new File(directoryPath);
+
+        // Check if the provided path is a directory
+        if (directory.isDirectory()) {
+            // Get list of files in the directory
+            File[] files = directory.listFiles();
+
+            // Add file names to the list
+            if (files != null) {
+                for (File file : files) {
+                    fileList.add(file.getName());
+                }
+            }
+        } else {
+            System.out.println("Provided path is not a directory.");
+        }
+
+        return fileList;
+    }
+
+	public void Server(){
+
+		int localhost_port = 9111;
+		ServerSocket serverSock = null;
+		ExecutorService service = null;
+
+		//create the log file
+		createNewFile("log.txt");
+
+		try {
+			serverSock = new ServerSocket(localhost_port);
+		}
+		catch(IOException IOE){
+			System.out.println( IOE );
+			System.exit(-1);
+		}
+
+		service = Executors.newFixedThreadPool(20);
+
+		running = true;
+        while( running ){
+
+			try{
+				//wait for connection
+				Socket clientSocket = serverSock.accept();
+			}
+			catch (IOException IOE) {
+				System.out.println( IOE );
+			}
+
+			try{
+				//get clientSocket info
+				InetAddress inet = clientSocket.getInetAddress();
+                Date date = new Date();
+
+				//open reader/writer for client request
+				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+				//read in request
+				String inputRequest = in.readLine();
+				//separate request by whitespace
+				String[] splitRequest = inputRequest.split("\\s+");
+
+				if(splitRequest[0] == "list"){
+					
+					//get list of files
+					String directoryPath = "serverFiles";
+					List<String> fileList = listFilesInDirectory(directoryPath);
+
+					//send list of files to client
+					for(int i = 0; i < fileList.size(); i++){
+						out.println(fileList[i]);
+					}
+
+				}
+				else if(splitRequest[0] == "put"){
+					String requestedFName = splitRequest[1];
+				}
+				else{
+
+				}
+				
+
+			}
+		}
+	}
+
 	public static void main( String[] args )
 	{
+		Server server = new Server();
+		server.Server();
 	}
 }
